7d1124bb7b9ca687f19af8c4dcae45c0d8571fc6
Sammatrice Lorenzo <lorenzosammatrice@gmail.com>
2022-02-14 22:58:14 +0100

    modif requete ajax

--- JS/fonctions.js
+++ JS/fonctions.js
@@ -1,34 +1,28 @@
 function insert()
 {
-    validation()
-
-    check = 0
-    if($("input:checked").length != 0) check = 1
-
-    $.ajax(
-        {
-            method:'GET',
-            url: "../PHP/ajout.php",
-            data: "mail="+$("#mail").val()+"&url="+$("#url").val()+"&check="+check,         
-            success: function(data)
-            {
-                $("#mail").val('')
-                $("#url").val('')
-                $("#check").val('')
-            },
-            errors: function()
-            {
-                $('.container')
-                alert("Erreur");
-            },
-        }
-    );
-}
-/**
- * Validation form
- */
-function validation(){
     if($("#mail").val() === "")  $("#email-feedback").append("Veuillez sasir l'adresse mail")
-
-    if ($("#url").val() === "")  ($("#url-feedback").append("Veuillez saiir l'url"))
+    else if($("#url").val() === "")  ($("#url-feedback").append("Veuillez saiir l'url")) 
+    else{
+        check = 0
+        if($("input:checked").length != 0) check = 1
+    
+        $.ajax(
+            {
+                method:'GET',
+                url: "../PHP/ajout.php",
+                data: "mail="+$("#mail").val()+"&url="+$("#url").val()+"&check="+check,         
+                success: function(data)
+                {
+                    $("#mail").val('')
+                    $("#url").val('')
+                    $("#check").val('')
+                },
+                errors: function()
+                {
+                    $('.container')
+                    alert("Erreur");
+                },
+            }
+        );
+    }
 }
