4ed9378fe72702d5fd675267a8727beb29ee383d
Sammatrice Lorenzo <lorenzosammatrice@gmail.com>
2022-02-17 11:22:43 +0100

    fix bugs

--- PHP/ajout.php
+++ PHP/ajout.php
@@ -4,8 +4,7 @@ $sql = $cnx->prepare("insert into user (mail, url, approbation, webcam, ecran, s
 
 $sql->bindValue(1, $_GET['mail']);
 $sql->bindValue(2, $_GET['url']);
-// $sql->bindValue(3, $_GET['check']);
-$sql->bindValue(3, 0);
+$sql->bindValue(3, $_GET['check']);
 $sql->bindValue(4, $_GET['webcam']);
 $sql->bindValue(5, $_GET['ecran']);
 $sql->bindValue(6, $_GET['slides']);
