49f1ecba3b7bbc6784cbb9fb86b930c345473933
Mattéo SCIMONE <mscimone@lerebours.org>
2022-02-17 11:41:10 +0100

    delete scrip.php

--- PHP/script.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    $sql = $cnx->prepare("select url from user order by DESC");
-    $sql->execute();
-    foreach($sql->fetchAll(PDO::FETCH_ASSOC) as $url) {
-        $commandes = shell_exec('bash capture-full-replay.sh' . $url['url']);
-    }
-?>
